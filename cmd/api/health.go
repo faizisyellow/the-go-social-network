25ca1976072e36a1d0ce2e0c2149fb231279ab2a
faizisyellow <muhamatfaissalm@gmail.com>
2025-05-26 06:18:31 +0700

    [add handler]

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -12,15 +12,6 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 		"env":     app.config.env,
 	}
 
-	if r.Header.Get("User-Agent") == "lizzy" {
-		err := WriteJSONError(w, http.StatusBadRequest, "user ga harus lizzy coy")
-		if err != nil {
-			// TODO: refactor
-			println(err)
-		}
-		return
-	}
-
 	err := writeJSON(w, http.StatusOK, data)
 	if err != nil {
 		// TODO: refactor
