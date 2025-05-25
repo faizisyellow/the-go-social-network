70c3f5655cde03757594e5eec7575ba174c7acda
faizisyellow <muhamatfaissalm@gmail.com>
2025-05-25 21:41:04 +0700

    [migrations]

---
+++ cmd/migrate/migrations/000002_create_posts_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS posts(
+    id int PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    user_id int NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
