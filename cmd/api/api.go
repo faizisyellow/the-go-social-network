ed5f2b694418c6feaa43643d471aab176a7767f5
faizisyellow <muhamatfaissalm@gmail.com>
2025-06-02 17:14:03 +0700

    add cors

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -12,10 +12,12 @@ import (
 
 	"faizisyellow.github.com/thegosocialnetwork/docs"
 	"faizisyellow.github.com/thegosocialnetwork/internal/auth"
+	"faizisyellow.github.com/thegosocialnetwork/internal/helpers"
 	"faizisyellow.github.com/thegosocialnetwork/internal/mailer"
 	"faizisyellow.github.com/thegosocialnetwork/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	httpSwagger "github.com/swaggo/http-swagger"
 	"go.uber.org/zap"
 )
@@ -77,6 +79,15 @@ func (app *application) mount() http.Handler {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{helpers.DefaultString(os.Getenv("CORS_ALLOWED_ORIGIN"), "http://localhost:5173")},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	// proccesing should be stop
 	r.Use(middleware.Timeout(60 * time.Second))
