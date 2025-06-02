27eacb00c8333e432048a5f9fa0ee1270bc63dba
faizisyellow <muhamatfaissalm@gmail.com>
2025-06-02 16:18:13 +0700

    clean

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,7 +12,6 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 		"env":     app.config.env,
 	}
 
-	time.Sleep(time.Second * 2)
 	err := app.jsonResponse(w, http.StatusOK, data)
 	if err != nil {
 		app.internalServerError(w, r, err)
